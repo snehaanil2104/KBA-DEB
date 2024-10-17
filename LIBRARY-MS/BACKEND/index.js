ac3ae047551193966bf8f79503be28ee4e2343d2
snehaanil2104 <snehaanil2104@gmail.com>
2024-10-17 23:37:53 +0530

    commit

--- LIBRARY-MS/BACKEND/index.js
+++ LIBRARY-MS/BACKEND/index.js
@@ -1,8 +1,33 @@
-import express from 'express';
-
+import express,{json} from 'express';
+import bcrypt from 'bcrypt';
 const app=express();
-const port=3000;
 
-app.listen(port,()=>{
-    console.log(`Server is listening to ${port}`)
+app.use(json())
+const port=3000;
+const user=new Map();
+
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+})
+
+app.post('/signup', async(req,res)=>{
+    console.log("Hi");
+    const data = req.body;
+    console.log(data.FirstName);
+
+    const{ FirstName,LastName,UserName,Password, Role}=data;
+    console.log(FirstName);
+    if(user.has(UserName)){
+        res.status(409).json({message:"already exist"})
+    }else{
+        const newP = await bcrypt.hash(Password,10)
+    console.log(newP);
+    user.set(UserName,{  FirstName,LastName,Password:newP,Role})
+    console.log(user.get(UserName));
+    res.status(201).json({message:"data saved"})
+    } 
+})
+
+app.listen(port,()=>{                                   
+    console.log(`Server is listening to port ${port}`)
 })
